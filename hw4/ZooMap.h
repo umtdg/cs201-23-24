8355d0f7e68ab68af647aaa441a5857735e0a457
Umut Dağ <umu7d9@gmail.com>
2023-12-15 14:32:52 +0300

    add ZooMap class structure

--- hw4/ZooMap.h
+++ hw4/ZooMap.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <string>
+
+class ZooMap {
+    public:
+        ZooMap(const std::string cageFile, const std::string zooFile);
+        ~ZooMap();
+
+        void displayAllCages() const;
+        void displayAdjacentCages(const std::string cageName) const;
+        void displayZooMap() const;
+
+        void findSafestPath(const std::string startCage, const std::string endCage);
+        void findMostDangerousPath(const std::string startCage, const std::string endCage);
+};
